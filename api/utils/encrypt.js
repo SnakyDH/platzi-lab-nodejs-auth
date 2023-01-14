93b8bcff41462499706b4e3deadc32a8c016d1a5
SnakyDH <skdanielg5@gmail.com>
2023-01-14 13:04:33 -0500

    [FIX] clean unnecessary console.log()

--- api/utils/encrypt.js
+++ api/utils/encrypt.js
@@ -1,8 +1,6 @@
 import bcryptjs from 'bcryptjs';
 const encrypt = async (text) => {
-  console.log(text);
   return await bcryptjs.hash(text, 10);
-  //console.log(gola);
 };
 const matching = async (text, encrypted) => {
   return await bcryptjs.compare(text, encrypted);
