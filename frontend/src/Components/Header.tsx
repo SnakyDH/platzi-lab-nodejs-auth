779f916c78f1e98676fa824a1b751f5e9397d338
SnakyDH <skdanielg5@gmail.com>
2023-04-25 14:33:27 -0500

    [FIX] bg and text config

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -3,16 +3,16 @@ export default function Header(): JSX.Element {
   return (
     <header className="sticky top-0 z-10 bg-indigo-800 py-3">
       <nav className="text-center">
-        <ul className="grid grid-cols-2 bg-red-500">
-          <li className="col-span-1 bg-red-400">
+        <ul className="grid grid-cols-2 font-bold">
+          <li className="col-span-1">
             <Link href="/">SnakyDH</Link>
           </li>
           <li className="col-span-1">
             <ul className="grid grid-cols-2">
-              <li className="bg-green-500 z-30">
+              <li className="z-30">
                 <Link href="/login">Login</Link>
               </li>
-              <li className="bg-slate-600">
+              <li className="h-full">
                 <Link href="/signup">Sign Up</Link>
               </li>
             </ul>
