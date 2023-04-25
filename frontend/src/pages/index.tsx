673b90135178e58ae68dcfb552b886b1834dc742
SnakyDH <skdanielg5@gmail.com>
2023-04-25 14:32:45 -0500

    [UPDATE] text of presentation

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -20,27 +20,28 @@ export default function Home() {
       </h1>
       <main className="container mx-auto my-5 flex flex-col gap-10 bg-slate-200 text-black rounded-lg p-5">
         <section>
-          <h2 className="text-3xl mb-10">About the project:</h2>
-          <p className="">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-            aliquam expedita rerum, dicta esse ab? Quia placeat iure quos
-            aliquid eius tempore, natus accusantium, ipsa vitae nulla aut?
-            Recusandae, eligendi? Iste maiores aliquam quasi laborum ut,
-            incidunt doloribus voluptatum deleniti, quaerat totam non distinctio
-            quis quo qui. Quos vitae totam minus? Voluptate aliquam repellat eos
-            assumenda saepe quia veritatis aperiam? Nisi omnis quos eum culpa in
-            suscipit nam, corporis accus dicta eos nulla reprehenderit, qui cum
-            asperiores suscipit impedit commodi maiores repellendus vero vel
-            doloribus doloremque! Unde quidem doloribus sed ipsam. Minus earum
-            similique aperiam laboriosam tempore us? Accusamus nostrum
-            distinctio unde iure fuga earum at molestiae a. Facere recusandae
-            nobis voluptatibus tempora dolores! Voluptatum expedita tenetur
-            corrupti itaque mollitia repudiandae esse quos repellat porro nobis
-            dicta vel est deleniti
+          <h2 className="text-3xl mb-10 font-bold">About the project:</h2>
+          <p className="text-lg mb-3">
+            Este proyecto fue la solución planteada por el autor
+            <Link href="https:/github.com/SnakyDH"> @SnakyDH</Link>, como solución al proyecto planteado por
+            <Link href="https://platzi.com"> @Platzi </Link>en el laboratorio de Node.js Autenticación y Seguridad, esta solución plantea el
+            uso de Json Web Tokens, para validar a los usuarios, los cuales estan guardados usando una base de datos MongoDB.
+          </p>
+          <p className='text-lg'>
+            Para el proyecto que estoy desarrollando, he decidido utilizar TailwindCSS y Next.js para la parte visual de la aplicación. 
+            TailwindCSS me permitirá diseñar y personalizar los estilos de manera eficiente y rápida, gracias a su extensa biblioteca 
+            de clases predefinidas. Además, Next.js me brindará la posibilidad de crear aplicaciones web escalables y optimizadas para 
+            SEO, gracias a su capacidad de renderización del lado del servidor.
+
+            En cuanto a la autenticación, he elegido utilizar JWT (JSON Web Tokens) para manejar la autenticación del usuario. Esto me 
+            permitirá generar tokens de acceso seguros y eficientes para el usuario, que podrán ser utilizados para acceder a ciertas 
+            funcionalidades de la aplicación. JWT también me brinda la posibilidad de transmitir información adicional en el token, como 
+            la fecha de expiración, lo que mejorará la seguridad de la aplicación. En resumen, el uso combinado de TailwindCSS, Next.js y 
+            JWT me permitirá crear una aplicación web visualmente atractiva, escalable y segura.
           </p>
         </section>
         <section>
-          <h2 className="text-3xl mb-10">Project technologies:</h2>
+          <h2 className="text-3xl mb-10 font-bold">Project technologies:</h2>
           <ul className="grid grid-cols-4">
             {tecnologies.map((tech) => (
               <li
@@ -49,7 +50,7 @@ export default function Home() {
               >
                 <Image
                   key={tech.id}
-                  className="flex self-center"
+                  className="flex self-center hover:animate-bounce"
                   width={50}
                   height={50}
                   src={tech.icon}
@@ -61,7 +62,7 @@ export default function Home() {
           </ul>
         </section>
         <section className="grid grid-rows-3">
-          <h2 className="row-span-1 text-3xl">Copyright:</h2>
+          <h2 className="row-span-1 text-3xl font-bold">Copyright:</h2>
           <div className="grid grid-cols-2 text-center">
             <small className="row-span-1 text-center">
               For images: &copy; Unsplash
@@ -72,7 +73,7 @@ export default function Home() {
       </main>
       <footer className="bg-indigo-800 text-center py-3">
         <Link className=" cursor-pointer" href="https://github.com/snakydh">
-          Make it With love ❤ Platzi 💚
+          Make it with love ❤ Platzi 💚
         </Link>
       </footer>
     </>
